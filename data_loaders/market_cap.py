b519d140c8ca0903891cf9e6890075d9f4e651ca
Brage G. Staven <bragestaven@gmail.com>
2018-02-14 09:01:13 +0100

    Lagde funksjon for  hente top x market cap crypto currencies

--- data_loaders/market_cap.py
+++ data_loaders/market_cap.py
@@ -2,13 +2,19 @@ from pymarketcap import *
 import pandas as pd
 
 coinmarketcap = Pymarketcap()
-all_coinmarketcap_coins = coinmarketcap.ticker(limit=0)
 
-df = pd.DataFrame(all_coinmarketcap_coins)
-df = df.sort_values('market_cap_usd', ascending=False)
-sorted_crypto_currencies = list(df.T.to_dict().values())
 
-filtered_crypto_currencies = sorted_crypto_currencies[:25]
+def get_top_x_market_cap(x):
+    all_coinmarketcap_coins = coinmarketcap.ticker(limit=0)
 
-for cc in filtered_crypto_currencies:
-    print(cc['name'])
+    df = pd.DataFrame(all_coinmarketcap_coins)
+    df = df.sort_values('market_cap_usd', ascending=False)
+    sorted_crypto_currencies = list(df.T.to_dict().values())
+
+    filtered_crypto_currencies = sorted_crypto_currencies[:x]
+
+    print('Top {} market cap crypto currencies'.format(x))
+    for cc in filtered_crypto_currencies:
+        print(cc['name'])
+
+    return filtered_crypto_currencies
